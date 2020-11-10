b69e19a21d34497298a5e3ed55c874368d0096b8
Nori <48003660+u3g@users.noreply.github.com>
2020-11-10 22:03:55 +0900

    add

--- map_filter-1.py
+++ map_filter-1.py
@@ -6,3 +6,6 @@ def myMap(f, arr):
 nums = [1,2,3,4,5,6,7]
 print(nums)
 print(myMap((lambda x: x*x),nums))
+
+# mapで返すのはイテレーター
+print(list(map(lambda x: x*x, nums)))
--- map_filter-2.py
+++ map_filter-2.py
@@ -5,3 +5,6 @@ def myFilter(predicateF, arr):
     return results
 list1 = [1,2,3,4,5,6,7,8,9,10]
 print(myFilter((lambda x: x%2 != 0), list1))
+
+# filterで返すのはイテレーター
+print(list(filter(lambda x: x%2 != 0, list1)))
